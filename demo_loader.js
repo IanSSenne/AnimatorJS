bc0c993cffae0134d2689f52eb2cb9047f5bdd76
IanSSenne <IanSSenne@gmail.com>
2019-04-02 20:51:43 -0700

    updated trigger for src to be src=true in search and base page for github.io

--- demo_loader.js
+++ demo_loader.js
@@ -1,6 +1,11 @@
-let src_0, src_1, src_2;
-let base = window.location.hostname == "localhost" ? window.location.origin : "https://ianssenne.github.io/AnimatorJS";
-if (window.location.search.length <= 1) {
+let src_0, src_1, src_2, base = window.location.hostname == "localhost" ? window.location.origin : "https://ianssenne.github.io/AnimatorJS",
+    URI = window.location.search.substr(1).split("&").reduce((a, b) => {
+        a[b.split("=")[0]] = JSON.parse((c = b.split("=", 2)[1]) == void 0 ? "false" : c);
+        return a;
+    }, {
+        src: false
+    });
+if (!URI.src) {
     src_0 = base + "/build/AnimatorJS.min.js"
     src_1 = base + "/build/AnimatorJS.dom.min.js"
     src_2 = base + "/build/AnimatorJS.ref.min.js"
